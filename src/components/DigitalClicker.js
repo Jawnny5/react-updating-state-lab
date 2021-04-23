f6f5a9f56777a3e26c7592f19830fb17b01cb8d9
“Jawnny5” <digitalanalogbros@gmail.com>
2021-04-23 08:13:27 -0600

    Deliverbles complete but in hooks

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React, { useState } from 'react'
+
+
+export default function DigitalClicker() {
+
+  const [timesClicked, setTimesClicked] = useState(0)
+  const handleClick = () => {
+    setTimesClicked(timesClicked + 1)
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <label>
+         {timesClicked}
+        </label>
+      </button>
+    </div>
+  )
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React, { useState } from 'react'
+
+export default function YouTubeDebugger() {
+
+  const [specs, setSpecs] = useState(
+    {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  )
+
+  const handleClick = (event) => {
+    setSpecs(() => {
+      return {...specs, settings: {
+        bitrate: '12',
+        video: {
+          resolution: '1080p'
+        }
+      }}
+    })
+  }
+  
+  const handleRes = (event) => {
+    setSpecs(() => {
+      return {...specs, settings: {
+        bitrate: '8',
+        video: {
+          resolution: '720p'
+        }
+      }}
+    })
+  }
+
+  return (
+    <div>
+      <button 
+        className='bitrate'
+        onClick={handleClick}
+      >   
+      </button>
+      <button 
+        className='resolution'
+        onClick={handleRes}
+      >   
+      </button>
+    </div>
+  )
+}
